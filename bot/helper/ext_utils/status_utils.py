79de57ec1998882476df7d430fee6bd274309396
BOSS1480 <yboss1480@gmail.com>
2025-06-29 05:56:37 +0300

    Update status_utils.py
    Signed-off-by: BOSS1480 <yboss1480@gmail.com>

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -288,10 +288,10 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
     if len(tasks) > STATUS_LIMIT:
         msg += f"<b>Page:</b> {page_no}/{pages} | <b>Tasks:</b> {tasks_no} | <b>Step:</b> {page_step}\n"
         buttons.data_button("⫷", f"status {sid} pre", position="header")
-        buttons.data_button("♻️ Refresh", f"status {sid} ref", position="header")
+        buttons.data_button("♻️", f"status {sid} ref", position="header")
         buttons.data_button("⫸", f"status {sid} nex", position="header")
     else:
-        buttons.data_button("♻️ Refresh", f"status {sid} ref", position="header")
+        buttons.data_button("♻️", f"status {sid} ref", position="header")
     
     # בניית התפריט עם 3 כפתורים בשורה
     button = buttons.build_menu(3)
