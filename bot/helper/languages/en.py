f1ddec48aae5b8c64c6ba2e508485334aea6026e
BOSS1480 <yboss1480@gmail.com>
2025-07-07 01:13:19 +0300

    Update en.py
    Signed-off-by: BOSS1480 <yboss1480@gmail.com>

--- bot/helper/languages/en.py
+++ bot/helper/languages/en.py
@@ -2,5 +2,5 @@ START_MSG = """
 <b>Tʜɪs Bᴏᴛ Cᴀɴ Mɪʀʀᴏʀ Aʟʟ Yᴏᴜʀ Lɪɴᴋs, Fɪʟᴇs Aɴᴅ Tᴏʀʀᴇɴᴛs Tᴏ Gᴏᴏɢʟᴇ Dʀɪᴠᴇ Oʀ Aɴʏ RCʟᴏɴᴇ Cʟᴏᴜᴅ Oʀ Tᴏ Tᴇʟᴇɢʀᴀᴍ Oʀ  DDL Sᴇʀᴠᴇʀs. 
 Tʏᴘᴇ /{cmd} Tᴏ Gᴇᴛ A Lɪsᴛ Oғ Aʟʟ Aᴠᴀɪʟᴀʙʟᴇ Cᴏᴍᴍᴀɴᴅs</b>
 """
-START_BUTTON1 = "𝙏𝙟 𝘽𝙤𝙩𝙨 🤖"
-START_BUTTON2 = "𝙂𝙧𝙤𝙪𝙥 🧲"
+START_BUTTON1 = "Tᴊ Bᴏᴛs 🤖"
+START_BUTTON2 = "Gʀᴏᴜᴘ 🧲"
