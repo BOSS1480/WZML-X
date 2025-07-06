06f777fecf3f39ff4d6d40d55b062a4e637dcb9e
BOSS1480 <yboss1480@gmail.com>
2025-07-07 00:50:31 +0300

    Update en.py
    Signed-off-by: BOSS1480 <yboss1480@gmail.com>

--- bot/helper/languages/en.py
+++ bot/helper/languages/en.py
@@ -2,5 +2,5 @@ START_MSG = """
 This bot can mirror from links|tgfiles|torrents|nzb|rclone-cloud to any rclone cloud, Google Drive or to telegram.
 Type /{cmd} to get a list of available commands
 """
-START_BUTTON1 = "Git Repo"
-START_BUTTON2 = "Updates"
+START_BUTTON1 = "𝐓𝐣 𝐁𝐨𝐭𝐬 🤖"
+START_BUTTON2 = "𝐆𝐫𝐨𝐮𝐩 🧲"
