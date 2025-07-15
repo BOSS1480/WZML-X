45cc7cf03ca83a842e691afa9d6c751d17291f64
BOSS1480 <yboss1480@gmail.com>
2025-07-15 22:07:09 +0300

    Update services.py
    Signed-off-by: BOSS1480 <yboss1480@gmail.com>

--- bot/modules/services.py
+++ bot/modules/services.py
@@ -32,9 +32,9 @@ async def start(_, message):
     lang = Language()
     buttons = ButtonMaker()
     buttons.url_button(
-        lang.START_BUTTON1, "https://t.me/Tj_Bots"
+        lang.START_BUTTON1, "https://t.me/Leech_Mirror_il"
     )
-    buttons.url_button(lang.START_BUTTON2, "https://t.me/Leech_Mirror_il")
+    buttons.url_button(lang.START_BUTTON2, "https://t.me/Tj_Leech_Group")
     reply_markup = buttons.build_menu(2)
 
     if len(message.command) > 1 and message.command[1] == "wzmlx":
