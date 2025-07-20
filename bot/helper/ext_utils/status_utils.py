b1ec979dc7b271350c88a1ce1a8cdd4f9ec5b039
BOSS1480 <yboss1480@gmail.com>
2025-07-20 04:46:54 +0300

    Update status_utils.py
    Signed-off-by: BOSS1480 <yboss1480@gmail.com>

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -287,7 +287,6 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
     if len(tasks) > STATUS_LIMIT:
         msg += f"<b>Page:</b> {page_no}/{pages} | <b>Tasks:</b> {tasks_no} | <b>Step:</b> {page_step}\n"
         buttons.data_button("⫷", f"status {sid} pre", position="header")
-        buttons.data_button("♻️", f"status {sid} ref", position="header")
         buttons.data_button(f"{page_no}/{pages}", f"status {sid} ref", position="header")
         buttons.data_button("⫸", f"status {sid} nex", position="header")
     else:
