8a622254799c5c9fcce1f5f4499c80b106d0f5d4
BOSS1480 <yboss1480@gmail.com>
2025-07-30 14:00:26 +0300

    Update status_utils.py
    Signed-off-by: BOSS1480 <yboss1480@gmail.com>

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -227,7 +227,7 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
         else:
             tstatus = task.status()
         msg += f"<b>{index + start_position}.</b> "
-        msg += f"<b><i>{escape(f'{task.name()}')}</i></b>"
+        msg += f"<blockquote><b><i>{escape(f'{task.name()}')}</i></b></blockquote>"
         if task.listener.subname:
             msg += f"\n┖ <b>Sub Name</b> → <i>{task.listener.subname}</i>"
         elapsed = time() - task.listener.message.date.timestamp()
